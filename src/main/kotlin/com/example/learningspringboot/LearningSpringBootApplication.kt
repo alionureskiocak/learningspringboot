eba64d7af0d67f9d9dd4a8ed37491ba3830ae057
Ali Onur Eskiocak <alionur3387@gmail.com>
2026-01-26 16:30:55 +0300

    Initial Commit

---
+++ src/main/kotlin/com/example/learningspringboot/LearningSpringBootApplication.kt
@@ -0,0 +1,11 @@
+package com.example.learningspringboot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LearningSpringBootApplication
+
+fun main(args: Array<String>) {
+	runApplication<LearningSpringBootApplication>(*args)
+}
